36bfdc2a6180be61b91044f5ca44913b6e233c2c
Naman <namansinghal198@gmail.com>
2019-09-19 16:10:39 +0530

    SIMECK ADDED

--- simeck.cpp
+++ simeck.cpp
@@ -9,8 +9,7 @@
 #include <fstream>
 #include <cryptominisat5/cryptominisat.h>
 
-// g++ simon.cpp -lcryptominisat5 -std=c++11
-
+// g++ simeck.cpp -lcryptominisat5 -std=c++11
 
 using CMSat::Lit;
 using CMSat::lbool;
@@ -20,7 +19,7 @@ using std::pair;
 
 
 // Just set N and M; 
-#define N (16)
+#define N (64)
 #define M (4)
 #define DEBUG (1)
 
@@ -91,72 +90,8 @@ using std::pair;
 // The Round Function
 uint64_t F(uint64_t x) 
 {
-	return (shift(x,1) & shift(x,8)) ^ shift(x,2);
+	return (shift(x,0) & shift(x,5)) ^ shift(x,1);
 }
-
-void test()
-{
-
-	uint64_t x, y, ex, ey;
-
-	if (N == 16 && M == 4) {
-		uint64_t keys[] = {0x0100, 0x0908, 0x1110, 0x1918};
-		set_key(keys);
-		x = 0x6565;	y = 0x6877;	ex = 0xc69b; ey = 0xe9bb;
-	}
-	if (N == 24 && M == 3) {
-		uint64_t keys[] = {0x121110, 0x0a0908, 0x020100};
-		set_key(keys);
-		x = 0x612067; y = 0x6e696c; ex = 0xdae5ac; ey = 0x292cac;
-	}
-	if (N == 24 && M == 4) {
-		uint64_t keys[] = {0x1a1918, 0x121110, 0x0a0908, 0x020100};
-		set_key(keys);
-		x = 0x726963; y = 0x20646e;	ex = 0x6e06a5; ey = 0xacf156;
-	}
-	if (N == 32 && M == 3) {
-		uint64_t keys[] = {0x03020100, 0x0b0a0908, 0x13121110};
-		set_key(keys);
-		x = 0x6f722067; y = 0x6e696c63; ex = 0x5ca2e27f; ey = 0x111a8fc8;		
-	}
-	if (N == 32 && M == 4) {
-		uint64_t keys[] = {0x03020100, 0x0b0a0908, 0x13121110, 0x1b1a1918};
-		set_key(keys);
-		x = 0x656b696c;	y = 0x20646e75; ex = 0x44c8fc20; ey = 0xb9dfa07a;		
-	}
-	if (N == 48 && M == 2) {
-		uint64_t keys[] = {0x050403020100, 0x0d0c0b0a0908};
-		set_key(keys);
-		x = 0x2072616c6c69; y = 0x702065687420; ex = 0x602807a462b4; ey = 0x69063d8ff082;
-	}
-	if (N == 48 && M == 3) {
-		uint64_t keys[] = {0x050403020100, 0x0d0c0b0a0908, 0x151413121110};
-		set_key(keys);
-		x = 0x746168742074; y = 0x73756420666f; ex = 0xecad1c6c451e; ey = 0x3f59c5db1ae9;
-	}	
-	if (N == 64 && M == 2) {
-		uint64_t keys[] = {0x0706050403020100, 0x0f0e0d0c0b0a0908};
-		set_key(keys);
-		x = 0x6373656420737265; y = 0x6c6c657661727420; ex = 0x49681b1e1e54fe3f; ey = 0x65aa832af84e0bbc;
-	}
-	if (N == 64 && M == 3) {	
-		uint64_t keys[] = {0x0706050403020100, 0x0f0e0d0c0b0a0908, 0x1716151413121110};
-		set_key(keys);
-		x = 0x206572656874206e; y = 0x6568772065626972; ex = 0xc4ac61effcdc0d4f; ey = 0x6c9c8d6e2597b85b;
-	}
-	if (N == 64 && M == 4) {
-		uint64_t keys[] = {0x0706050403020100, 0x0f0e0d0c0b0a0908, 0x1716151413121110, 0x1f1e1d1c1b1a1918};
-		set_key(keys);
-		x = 0x74206e69206d6f6f; y = 0x6d69732061207369; ex = 0x8d2b5579afc8a3a0; ey = 0x3bf72a87efe7b868;
-	}
-	
-	encrypt(x, y);
-	if (x != ex || y != ey)
-		printf("Test-std::vector mismatch! %016lx %016lx <=> %016lx %016lx\n", x, y, ex, ey);
-	else
-		printf("**Test Successful! Ready for Attacks**\n");
-}
-
 
 ////////////////////////////////////////
 /////////////// DFA ////////////////////
@@ -174,14 +109,15 @@ void test()
 ////////////// SAT SETUP //////////////
 ///////////////////////////////////////
 
+
 #include "sat_functions.h"
 
 // Add Equations given the Fault is in Round R given the xor of L register of round R+1 or R redister of R+2
 void equations_2nd_round(SATSolver &solver,uint64_t lxor,uint64_t k,uint64_t &var_counter, std::map<uint64_t,uint64_t>&vars)
 {
 	uint64_t c,d;
-	uint64_t A = ((k-7+N)%N);
-	uint64_t B = ((k+7+N)%N);
+	uint64_t A = ((k-5+N)%N);
+	uint64_t B = ((k+5+N)%N);
 
 	if(vars.find(A) != vars.end())
 	{
@@ -203,16 +139,16 @@ void equations_2nd_round(SATSolver &solver,uint64_t lxor,uint64_t k,uint64_t &va
 		var_counter++;
 	}
 
-	set_sat(solver, c, lxor & (0x1ull<< ((k+1)%N))? true : false );
-	set_sat(solver, d, lxor & (0x1ull<< ((k+8)%N))? true : false );
+	set_sat(solver, c, lxor & (0x1ull<< ((k)%N))? true : false );
+	set_sat(solver, d, lxor & (0x1ull<< ((k+5)%N))? true : false );
 }
 
 // Add Equations given the Fault is in Round R given the xor of L register of round R+2 or R redister of R+3
 void equations_3rd_round(SATSolver &solver,uint64_t lxor, uint64_t k, uint64_t &var_counter, std::map<uint64_t,uint64_t>&vars)
 {
 	uint64_t c,d;
-	uint64_t A = ((k-7+N)%N);
-	uint64_t B = ((k+7+N)%N);
+	uint64_t A = ((k-5+N)%N);
+	uint64_t B = ((k+5+N)%N);
 
 	if(vars.find(A) != vars.end())
 	{
@@ -234,38 +170,31 @@ void equations_3rd_round(SATSolver &solver,uint64_t lxor, uint64_t k, uint64_t &
 		var_counter++;
 	}
 
-	and_sat(solver, ((k-6+N)%N), c, var_counter);
-	set_sat(solver, var_counter, lxor & (0x1ull<< ((k+2)%N))? true : false );
+	and_sat(solver, ((k-5+N)%N), c, var_counter);
+	set_sat(solver, var_counter, lxor & (0x1ull<< ((k)%N))? false : true );
 	var_counter++;
 
-	if((k+16)%N != k)
-	{		
-		and_sat(solver, ((k+15)%N), d, var_counter);
-		set_sat(solver, var_counter, lxor & (0x1ull<< ((k+16)%N))? true : false );
-	}else
-	{
-		and_sat(solver, ((k+15)%N), d, var_counter);
-		set_sat(solver, var_counter, lxor & (0x1ull<< ((k+16)%N))? false : true );
-	}
+	and_sat(solver, ((k+10)%N), d, var_counter);
+	set_sat(solver, var_counter, lxor & (0x1ull<< ((k+10)%N))? true : false );
 	var_counter++;
 
-	xor_sat(solver, ((k-5+N)%N), c, var_counter);
-	set_sat(solver, var_counter, lxor & (0x1ull<< ((k+3)%N))? true : false);
+	xor_sat(solver, ((k-4+N)%N), c, var_counter);
+	set_sat(solver, var_counter, lxor & (0x1ull<< ((k+1)%N))? true : false);
 	var_counter++;
 
-	xor_sat(solver, ((k+9+N)%N), d, var_counter);
-	set_sat(solver, var_counter, lxor & (0x1ull<< ((k+10)%N))? true : false);
+	xor_sat(solver, ((k+6+N)%N), d, var_counter);
+	set_sat(solver, var_counter, lxor & (0x1ull<< ((k+6)%N))? true : false);
 	var_counter++;
 
-	xor_sat(solver, ((k+1)%N), c, var_counter);
+	xor_sat(solver, ((k)%N), c, var_counter);
 	uint64_t ta = var_counter;
 	var_counter++;
 
-	xor_sat(solver, ((k+8)%N), d, var_counter);
+	xor_sat(solver, ((k+5)%N), d, var_counter);
 	uint64_t tb = var_counter;
 	var_counter++;
 
-	and_sat(solver, ((k+1)%N), ((k+8)%N), var_counter);
+	and_sat(solver, ((k)%N), ((k+5)%N), var_counter);
 	uint64_t tc = var_counter;
 	var_counter++;
 
@@ -274,11 +203,9 @@ void equations_3rd_round(SATSolver &solver,uint64_t lxor, uint64_t k, uint64_t &
 	var_counter++;
 
 	xor_sat(solver, tc, td, var_counter);
-	set_sat(solver, var_counter, lxor & (0x1ull<< ((k+9)%N)) ? true : false);
+	set_sat(solver, var_counter, lxor & (0x1ull<< ((k+5)%N)) ? true : false);
 	var_counter++;
 }
-
-uint64_t fault_number[M];
 
 // Mounts the Attack on a Single Round
 void dfa_attack(uint64_t a, uint64_t b, bool found[N+1][M+2], uint64_t L_reg[M+2])
@@ -459,8 +386,6 @@ void dfa_attack(uint64_t a, uint64_t b, bool found[N+1][M+2], uint64_t L_reg[M+2
         }
 	}
 
-	fault_number[T-2] = attack_pos.size();
-
 	return;
 }
 
@@ -554,69 +479,6 @@ void complete_dfa()
 }
 
 
-/////////////////////////////////////////////////////////
-////////////// Randon Functions Currently Testing /////////
-/////////////////////////////////////////////////////////
-
-// Function Written to find relation between hamming weight
-// encrypted text and previous round registers
-// Current Result: No Relations
-void check_hamming_relation()
-{
-	uint64_t a,b,x,y,x1,y1;
-
-	uint64_t s1,s2,s3;
-	s1 = s2 = s3 = 0;
-
-	int64_t s4 = 0;
-
-	int c1,c2,c3,c4;
-
-	c4 = 0;
-
-
-	for(int i=0;i<1e7;i++)
-	{
-
-		a = uni_dist(rng) & WORD_MASK;
-		b = uni_dist(rng) & WORD_MASK;
-		setup_random_key();
-
-		x1 = x = a;
-		y1 = y = b;
-		encrypt(x, y, ROUNDS);
-		encrypt(x1, y1, ROUNDS-1);
-		// std::cout<<hammingWeight(x)<<' ';
-		// std::cout<<hammingWeight(y)<<' ';
-		// std::cout<<hammingWeight(y1)<<'\n';
-		
-
-		c1 = hammingWeight(x);
-		c2 = hammingWeight(y);
-		c3 = hammingWeight(y1);
-	
-
-		if(c1 >= 14)
-		{
-
-			s1 += c1;
-			s2 += c2;
-			s3 += c3;
-			c4++;
-
-		}
-	}
-
-	if(c4 != 0)
-	{
-		std::cout<<s1/c4<<' ';
-		std::cout<<s2/c4<<' ';
-		std::cout<<s3/c4<<'\n';
-	}
-}
-
-		
-
 int main() {
 
 	rng.seed(time(NULL)); // seed marsenne twister rng
@@ -628,9 +490,7 @@ int main() {
 	// uint64_t hi;
 
 	// printf("\nllo\n");
-
-	test();
-	test_cypher();
+	// test_cypher();
 	// uint64_t keys[M] = {0x0100, 0x0908, 0x1110, 0x1918};
 	// set_key(keys);
 
@@ -638,41 +498,22 @@ int main() {
 	x = 0x6565;	y = 0x6877;	ex = 0xc69b; ey = 0xe9bb;
 	faulty_encrypt(x, y, 8);
 	printf("%lx, %lx \n", x, y);
-
 	printf("%s, %s \n", binary(x), binary(y));
-
-
 
 	xf = 0x6565;	yf = 0x6877;
 	
-	// encrypt(xf, yf);
-	// printf("%lx, %lx \n", xf, yf);
-	// printf("%s, %s \n", binary(xf), binary(yf));
-	// printf("%s, %s \n", binary(x^xf), binary(y^yf));
+	encrypt(xf, yf);
+	printf("%lx, %lx \n", xf, yf);
+	printf("%s, %s \n", binary(xf), binary(yf));
 
 	fault_location_find_setup();
 	// fault_location_find_test();
+	// dfa_attack_2nd_last_round();
 
-	for(uint64_t i=0;i<1e1;i++)
-	{
-		for(uint64_t j=0;j<M;j++)
-		{
-			fault_number[j] = 0;
-		}
-		complete_dfa();
+	complete_dfa();
 
-		uint64_t sum = 0;
-
-		for(uint64_t j=0;j<M;j++)
-		{
-			std::cout<<fault_number[j]<<' ';
-			sum += fault_number[j];
-		}std::cout<<sum<<std::endl;
-
-		break;
-	}
-
-	// check_hamming_relation();
+	// dfa_attack_1();
+	// sat_solver();
 
 	return 0;
 }
